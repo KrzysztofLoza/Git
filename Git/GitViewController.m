ff1fef5cdac387c04f56153c70a1db8e0712a0bb
KrzysztofLoza <kanclerz86@gmail.com>
2011-07-22 12:16:49 +0200

    Removed viewDidAppear

--- Git/GitViewController.m
+++ Git/GitViewController.m
@@ -24,9 +24,6 @@
 }
 
 #pragma mark - View lifecycle
-- (void)viewDidAppear:(BOOL)animated{
-	[super viewDidAppear:animated];
-}
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
